555d7fd2d44c450ab4ac37dbb959fab4b391e771
Emmanuel Saleem <101662490+es-77@users.noreply.github.com>
2025-04-14 13:02:44 +0000

    relationship or seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        User::factory(10)
+        ->has(
+            Post::factory()
+                ->count(5)
+                ->has(Comment::factory()->count(3), 'comments'),
+            'posts'
+        )
+        ->create();
 
         User::factory()->create([
             'name' => 'Test User',
